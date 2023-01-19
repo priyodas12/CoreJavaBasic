6f00cf93dcffba4124555a6f93783d92d8dfe615
Priyobrato.Das <priyodas12@gmail.com>
2023-01-19 21:57:01 +0530

    basic programs:java 8-completableFuture 2

--- src/java8/completableFuture/RunAsyncImpl.java
+++ src/java8/completableFuture/RunAsyncImpl.java
@@ -2,23 +2,24 @@ package java8.completableFuture;
 
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
-public class CompletableFutureMain {
+public class RunAsyncImpl {
 
     public static void main(String[] args) {
         System.out.println("main:Execution started by "+Thread.currentThread().getName());
-        //supplyAsync takes a supplier
-        CompletableFuture.supplyAsync(CompletableFutureMain::generateSomeNumber)
-                .thenAccept(val-> System.out.println(val+"printed by"+Thread.currentThread().getName()));
+        //runAsync takes a supplier
+        CompletableFuture.runAsync(()->generateSomeNumber());
         sleepSomeTime(4000);
         System.out.println("main:Execution finished by "+Thread.currentThread().getName());
     }
+
+
     public static int generateSomeNumber(){
         sleepSomeTime(3000);
         System.out.println("generateSomeNumber:Executed by "+Thread.currentThread().getName());
         return new Random().nextInt();
     }
+
 
     public static void sleepSomeTime(int times) {
         try {
