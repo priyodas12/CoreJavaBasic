ae0bcc290516f7afd34709f4c29a2bf7375e911c
Priyobrato.Das <priyodas12@gmail.com>
2023-02-02 23:09:59 +0530

    parallel processing

---
+++ src/threads/MasterNode.java
@@ -0,0 +1,89 @@
+package threads;
+
+import java.util.concurrent.TimeUnit;
+
+public class MasterNode {
+    public static void main(String[] args) {
+
+/*        WorkerNode1 n1=new WorkerNode1();
+        WorkerNode2 n2=new WorkerNode2();
+
+        long startTime=System.nanoTime();
+        try {
+            n1.executeTask();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e+"from WorkerNode1");
+        }
+        try {
+            n2.executeTask();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e+"from WorkerNode2");
+        }
+        long endTime=System.nanoTime();
+        System.out.println("TotalTime Taken:  "+(endTime-startTime)/1000000000);*/
+
+        ParallelWorkerNode1 pn1=new ParallelWorkerNode1();
+        ParallelWorkerNode2 pn2=new ParallelWorkerNode2();
+
+        long startTime=System.nanoTime();
+        System.out.println(Thread.currentThread().getName()+">>"+startTime);
+        pn1.start();
+        pn2.start();
+
+        try {
+            pn1.join();
+            pn2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        long endTime=System.nanoTime();
+        System.out.println(Thread.currentThread().getName()+">>"+endTime);
+        System.out.println("TotalTime Taken:  "+(endTime-startTime)/1000000000);
+
+    }
+}
+
+class WorkerNode1{
+    public void executeTask() throws InterruptedException {
+        for (int i = 0; i <10 ; i++) {
+            TimeUnit.MILLISECONDS.sleep(100);
+            System.out.println(this.getClass()+" executing task number"+i);
+        }
+    }
+}
+class WorkerNode2{
+    public void executeTask() throws InterruptedException {
+        for (int i = 0; i <10 ; i++) {
+            TimeUnit.MILLISECONDS.sleep(100);
+            System.out.println(this.getClass()+" executing task number"+i);
+        }
+    }
+}
+
+class ParallelWorkerNode1 extends Thread{
+    @Override
+    public void run() {
+        for (int i = 0; i <10 ; i++) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(this.getClass()+" executing task number"+i);
+        }
+    }
+}
+
+class ParallelWorkerNode2 extends Thread{
+    @Override
+    public void run() {
+        for (int i = 0; i <10 ; i++) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(this.getClass()+" executing task number"+i);
+        }
+    }
+}
